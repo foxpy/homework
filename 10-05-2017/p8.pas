331e6396ae9d80d4f18bef63587ad76b91be8bdf
foxpy <muradkarammaev@gmail.com>
2017-10-04 17:34:36 +0300

    Tasks 8,9 completed.

--- 10-05-2017/p8.pas
+++ 10-05-2017/p8.pas
@@ -2,8 +2,8 @@ VAR
     a : REAL;
 BEGIN
     readln(a);
-    write('S: ', a*a, ' ');
-    write('Sside: ', 6*a*a, ' ');
-    writeln('V: ', a*a*a);
+    write('S: ', a*a:10:3, ' ');
+    write('Sside: ', 6*a*a:10:3, ' ');
+    writeln('V: ', a*a*a:10:3);
 END.
 
--- 10-05-2017/p9.pas
+++ 10-05-2017/p9.pas
@@ -2,7 +2,7 @@ VAR
     a, b, c : REAL;
 BEGIN
     readln(a, b, c);
-    writeln((a+b+c) / 3);
-    writeln(sqrt(a*b*c));
+    writeln((a+b+c) / 3:10:3);
+    writeln(sqrt(a*b*c):10:3);
 END.
 
