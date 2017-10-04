b98227283a8b676a00a21ee0276944fc6befa93d
foxpy <muradkarammaev@gmail.com>
2017-10-04 17:05:08 +0300

    Task 5 completed.

--- 10-05-2017/p5.pas
+++ 10-05-2017/p5.pas
@@ -1,9 +1,9 @@
-USES math;
-
+USES
+    MATH;
 VAR
     a, b, al : REAL;
 BEGIN
     read(a, b, al);
-    writeln('S: ', tan(al)*(a-b)/2*(a + b)/2);
+    writeln('S: ', tan(al*pi/180)*(sqr(a) - sqr(b))/2:10:3);
 END.
 
