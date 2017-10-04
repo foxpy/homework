99cb07c69a37bef07f780eda112be59b15c17c65
foxpy <muradkarammaev@gmail.com>
2017-10-04 16:17:55 +0300

    Task 1 completed.

--- 10-05-2017/p1.pas
+++ 10-05-2017/p1.pas
@@ -2,8 +2,8 @@ VAR
     a, b : REAL;
 BEGIN
     readln(a, b);
-    write('c: ', sqrt(a*a + b*b):3:5, ' ');
-    write('P: ', a + b + sqrt(a*a + b*b):3:5, ' ');
-    writeln('S: ', a*b/2:3:5);
+    write('c: ', sqrt(a*a + b*b):10:3, ' ');
+    write('P: ', a + b + sqrt(a*a + b*b):10:3, ' ');
+    writeln('S: ', a*b/2:10:3);
 END.
 
