11ffa88e310adfc4a110fffd3157adbd869709c1
foxpy <muradkarammaev@gmail.com>
2018-02-27 20:59:02 +0300

    Revert bad fix.

--- semester 2/programming languages/02-14-2018/2.py
+++ semester 2/programming languages/02-14-2018/2.py
@@ -1,13 +1,6 @@
 def beautify(input_str, punctuation_marks=['.', ',', '!', '?']):
-    def parse_punctuation(s):
-        r = ""
-        for i in s:
-            r += (i + " ") if i in punctuation_marks else i
-        print(r)
-        return r
-
     output_str = ""
     for i in input_str.split():
-        output_str += i if i in punctuation_marks else parse_punctuation(i)
+        output_str += (" " + i) if i not in punctuation_marks else i
     return output_str.strip()
 
