5c6b7e8d7f12a8abed6521966dcb71bc1841bee4
foxpy <muradkarammaev@gmail.com>
2018-03-23 18:27:51 +0300

    Add more whitespace between functions.

--- semester 2/programming languages/03-21-2018/sorting.py
+++ semester 2/programming languages/03-21-2018/sorting.py
@@ -5,6 +5,7 @@ def bubble_sort(array):
                 array[j], array[j + 1] = array[j + 1], array[j]
     return array
 
+
 def insertion_sort(array):
     for i in range(1, len(array)):
         j = i
@@ -12,6 +13,7 @@ def insertion_sort(array):
             array[j], array[j-1] = array[j-1], array[j]
             j -= 1
     return array
+
 
 def quick_sort(array):
     if len(array) > 1:
