d0f14a2f68f8a5c9dfe8ffe1904e9b17ee88b7ce
foxpy <muradkarammaev@gmail.com>
2018-04-04 10:25:38 +0300

    Add Bellman-Ford algorythm

--- semester 2/discrete math/03-26-2018/algorithms.py
+++ semester 2/discrete math/03-26-2018/algorithms.py
@@ -27,3 +27,22 @@ def dijkstra_shortest_pathways(adjacency_matrix, number_of_nodes, start_node_num
                 distance[node] = distance[min_weight_index] + adjacency_matrix[min_weight_index][node]
         unchecked[min_weight_index] = False
     return distance
+
+
+def bellman_ford_shortest_pathways(number_of_nodes, edges, weight_matrix, start_node_number):
+    """Find the shortest pathway to every node in the graph.
+
+    Positional arguments:
+    edges -- array of tuples representing edges.
+    e. g. [(0, 1), (0, 2), (1, 2), (2, 0)].
+    weight_matrix -- two-dimensional array of distances between nodes."""
+
+    distance = [float('inf')]*number_of_nodes
+    distance[start_node_number] = 0
+
+    for i in range(number_of_nodes):
+        for u, v in edges:
+            if distance[v] > distance[u] + weight_matrix[u][v]:
+                distance[v] = distance[u] + weight_matrix[u][v]
+
+    return distance
