6ade8a20edd0ad5413f79ee20c43bccbb29c4fce
foxpy <muradkarammaev@gmail.com>
2018-04-08 20:56:15 +0300

    Implement Bellman Ford algorithm again But with breadth-first search, also mark work as finished.

--- semester 2/discrete math/03-26-2018/algorithms.py
+++ semester 2/discrete math/03-26-2018/algorithms.py
@@ -46,3 +46,39 @@ def bellman_ford_shortest_pathways(number_of_nodes, edges, weight_matrix, start_
                 distance[v] = distance[u] + weight_matrix[u][v]
 
     return distance
+
+
+def bellman_ford_shortest_pathways_bfs(number_of_nodes, adjacency_list, weight_matrix, start_node_number):
+    """Find the shortest pathway to every node in the graph.
+    Uses breadth-first search.
+
+    Positional arguments:
+    adjacency_list -- two-dimensinal array, represents adjacency of nodes.
+    e. g. [[1, 2],
+           [0, 3],
+           [0, 3],
+           [1, 2]].
+    weight_matrix -- two-dimensional array, represents distance between nodes.
+    e. g. [[0, 4, 8, float('inf')],
+           [4, 0, float('inf'), 20],
+           [8, float('inf'), 0, 4],
+           [float('inf'), 20, 4, 0]]."""
+
+    distance = [float('inf')]*number_of_nodes
+    distance[start_node_number] = 0
+
+    checked = set()
+    queue = {start_node_number}
+
+    while queue:
+        node = queue.pop()
+        checked.add(node)
+
+        queue.update(adjacency_list[node])
+        queue -= checked
+
+        for next_node in adjacency_list[node]:
+            if distance[node] > distance[next_node] + weight_matrix[next_node][node]:
+                distance[node] = distance[next_node] + weight_matrix[next_node][node]
+
+    return distance
