c3fa94a768ef62f8b86217b9b8580bc4fa92992d
foxpy <muradkarammaev@gmail.com>
2018-04-24 13:07:27 +0300

    Implement sligtly optimized insertion sort. However, it should be slower in Python.

--- semester 2/programming languages/03-21-2018/sorting.py
+++ semester 2/programming languages/03-21-2018/sorting.py
@@ -15,6 +15,17 @@ def insertion_sort(array):
     return array
 
 
+def another_yet_insertion_sort(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i-1
+        while j >= 0 and array[j] > key:
+            array[j+1] = array[j]
+            j -= 1
+        array[j+1] = key
+    return array
+
+
 def selection_sort(array):
     for i in range(len(array)-1):
         selection = i
