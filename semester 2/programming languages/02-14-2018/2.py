f7edd5851a27cc7e3d48d676a1ef66a8b2c2036c
foxpy <muradkarammaev@gmail.com>
2018-05-03 21:07:18 +0300

    Expand punctuation marks list in old homework task Noone cares about it, and I do not as well.

--- semester 2/programming languages/02-14-2018/2.py
+++ semester 2/programming languages/02-14-2018/2.py
@@ -1,4 +1,6 @@
-def beautify(input_str, punctuation_marks=['.', ',', '!', '?']):
+from string import punctuation
+
+def beautify(input_str, punctuation_marks=punctuation):
     output_str = ""
     for i in input_str.split():
         output_str += (" " + i) if i not in punctuation_marks else i
