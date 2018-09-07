271b9e1f66c6105fe28f7aec08d43b27c8a6feb7
Murad <muradkarammaev@gmail.com>
2018-09-07 12:56:44 +0300

    Calculate discriminant
    Throw an error if discriminant is less than zero

--- semester 3/programming languages/09-07-2018/2.c
+++ semester 3/programming languages/09-07-2018/2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	float A, B, C;
+	float A, B, C, D;
 	puts("Ax^2+Bx+C=0");
 
 	printf("Enter A: ");
@@ -13,6 +13,16 @@ int main(int argc, char *argv[]) {
 
 	printf("Enter C: ");
 	scanf("%f", &C);
+
+	D = B*B - 4*A*C;
+
+	if (D < 0) {
+		puts("Sorry. I cannot solve this equation.");
+		puts("Basically because I know nothing about imaginary numbers. :)");
+		exit(1);
+	}
+
+	printf("Calculated D: %f.\n", D);
 
 	return 0;
 }
