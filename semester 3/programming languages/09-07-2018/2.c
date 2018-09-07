efc916f6ca89e8fd69265195b4f461009c20eb3e
Murad <muradkarammaev@gmail.com>
2018-09-07 12:45:03 +0300

    Read A, B and C from stdin

---
+++ semester 3/programming languages/09-07-2018/2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	float A, B, C;
+	puts("Ax^2+Bx+C=0");
+
+	printf("Enter A: ");
+	scanf("%f", &A);
+
+	printf("Enter B: ");
+	scanf("%f", &B);
+
+	printf("Enter C: ");
+	scanf("%f", &C);
+
+	return 0;
+}
