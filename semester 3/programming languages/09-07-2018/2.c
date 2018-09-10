a9ed7792b1e6c9830382efc0db42856cb44670aa
Murad <muradkarammaev@gmail.com>
2018-09-10 12:43:54 +0300

    Calculate complex numbers answer

--- semester 3/programming languages/09-07-2018/2.c
+++ semester 3/programming languages/09-07-2018/2.c
@@ -16,18 +16,20 @@ int main(int argc, char *argv[]) {
 	scanf("%f", &C);
 
 	D = B*B - 4*A*C;
+	puts("Answers:");
 
 	if (D < 0) {
-		puts("Sorry. I cannot solve this equation.");
-		puts("Basically because I know nothing about complex numbers. :)");
-		exit(1);
+		// We are basically reusing existing variable for imaginary part
+		x1 = -B/2/A;
+		x2 = sqrt(fabs(D))/2/A;
+		printf("x1: %f+%fi,\nx2: %f-%fi.\n", x1, x2, x1, x2);
+		return 0;
 	}
 
 	x1 = (-B + sqrt(D))/2/A;
 	x2 = (-B - sqrt(D))/2/A;
 
-	puts("Answers:");
-	printf("x1: %f\nx2: %f.\n", x1, x2);
+	printf("x1: %f,\nx2: %f.\n", x1, x2);
 
 	return 0;
 }
