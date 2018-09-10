f92ebabb76ffe79d07a9e5608548607c7f45a792
Murad <muradkarammaev@gmail.com>
2018-09-10 12:49:27 +0300

    Define variables only when needed

--- semester 3/programming languages/09-07-2018/2.c
+++ semester 3/programming languages/09-07-2018/2.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 int main(int argc, char *argv[]) {
-	float A, B, C, D, x1, x2;
+	float A, B, C, D;
 	puts("Ax^2+Bx+C=0");
 
 	printf("Enter A: ");
@@ -19,15 +19,14 @@ int main(int argc, char *argv[]) {
 	puts("Answers:");
 
 	if (D < 0) {
-		// We are basically reusing existing variable for imaginary part
-		x1 = -B/2/A;
-		x2 = sqrt(fabs(D))/2/A;
-		printf("x1: %f+%fi,\nx2: %f-%fi.\n", x1, x2, x1, x2);
+		float re = -B/2/A;
+		float im = sqrt(fabs(D))/2/A;
+		printf("x1: %f+%fi,\nx2: %f-%fi.\n", re, im, re, im);
 		return 0;
 	}
 
-	x1 = (-B + sqrt(D))/2/A;
-	x2 = (-B - sqrt(D))/2/A;
+	float x1 = (-B + sqrt(D))/2/A;
+	float x2 = (-B - sqrt(D))/2/A;
 
 	printf("x1: %f,\nx2: %f.\n", x1, x2);
 
