a6dad52c5214ff0671078968f74fcc763a465a5f
Murad <muradkarammaev@gmail.com>
2019-03-19 15:14:54 +0300

    extract UTF-8 character number for 2-byte character and print it out

--- semester 4/programming languages/03-01-2019/3.cpp
+++ semester 4/programming languages/03-01-2019/3.cpp
@@ -5,6 +5,10 @@
 #include <langinfo.h>
 #include <string.h>
 
+#include <cstdint>
+#include <stdio.h>
+#include <bitset>
+
 int main()
 {
 	// That's mostly C code.. in the C++ project. I don't really like it.
@@ -17,13 +21,19 @@ int main()
 	std::string input, output;
 	std::getline(std::cin, input);
 
+	std::uint16_t sequence = 0;
+
 	int i = 0;
 	while(i < input.length()) {
 		unsigned char c = *((unsigned char*) &input[i]);
+		unsigned char cc = *((unsigned char*) &input[i+1]);
 		if(c < 0x80) {
 			output.append(input, i, 1);
 			i += 1;
 		} else if(c < 0xe0) {
+			sequence = ((c & 0b00011111) << 6) | (cc & 0b00111111);
+			std::bitset<16> b(sequence);
+			std::cout << b << " " << std::hex << "0x" << sequence << std::endl;
 			output.append(input, i, 2);
 			i += 2;
 		} else if(c < 0xf0) {
@@ -40,6 +50,7 @@ int main()
 
 	std::cout << "Input:  " << input << std::endl <<
 	             "Output: " << output << std::endl;
+
 
 	return EXIT_SUCCESS;
 }
