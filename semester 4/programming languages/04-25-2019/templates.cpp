16db6fd8b594d2a60906a886be1d56a6fcd47a9a
Murad <muradkarammaev@gmail.com>
2019-03-21 19:36:54 +0300

    implement multiply_by_min()

--- semester 4/programming languages/04-25-2019/templates.cpp
+++ semester 4/programming languages/04-25-2019/templates.cpp
@@ -6,12 +6,22 @@
 
 #include "templates.hpp"
 
+#define ARRAY {2, -4, 0, 8}
+#define ARRAY_SIZE 4
+
 template<class T>
 void multiply_by_min(T& array, size_t len)
 {
+	long long min = array[0];
+	// how do I get the type of array passed?
+	// long long is good, but it may be slower on some systems
+	// e. g. on i386 or armv7h
 	size_t i;
 	for (i = 0; i < len; i++) {
-		array[i] *= i;
+		if (array[i] < min) min = array[i];
+	}
+	for (i = 0; i < len; i++) {
+		array[i] *= min;
 	}
 }
 
@@ -26,16 +36,19 @@ void show_array(T& array, size_t len)
 
 int main()
 {
-	int test1[4] = {1, 2, 3, 4};
-	std::array<int, 4> test2 {1, 2, 3, 4};
-	std::vector<int> test3 = {1, 2, 3, 4};
+	int test1[ARRAY_SIZE] = ARRAY;
+	std::array<int, ARRAY_SIZE> test2 ARRAY;
+	std::vector<int> test3 = ARRAY;
 
+	std::cout << "Working on C array:" << std::endl;
 	multiply_by_min(test1, 4);
 	show_array(test1, 4);
 
+	std::cout << "Working on C++ array:" << std::endl;
 	multiply_by_min(test2, 4);
 	show_array(test2, 4);
 
+	std::cout << "Working on C++ vector:" << std::endl;
 	multiply_by_min(test3, 4);
 	show_array(test3, 4);
 	return EXIT_SUCCESS;
