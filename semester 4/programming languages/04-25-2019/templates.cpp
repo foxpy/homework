cb4a9fe9e3723f74995066d2f4c1343624161921
Murad <muradkarammaev@gmail.com>
2019-03-22 10:07:31 +0300

    implement class template

--- semester 4/programming languages/04-25-2019/templates.cpp
+++ semester 4/programming languages/04-25-2019/templates.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdlib>
 #include <cstdint>
+#include <string>
 
 #include "templates.hpp"
 
@@ -47,33 +48,79 @@ void show_array(T& array, size_t len)
 	}
 }
 
+template<class T>
+void Stack<T>::push(T const& element)
+{
+	stack.push_back(element);
+}
+
+template<class T>
+T Stack<T>::pop()
+{
+	auto back = stack.end();
+	back--;
+	if (stack.empty()) {
+		throw std::out_of_range("empty stack");
+	} else {
+		stack.pop_back();
+		return *back;
+	}
+}
+
 int main()
 {
-	int test1[ARRAY_SIZE] = ARRAY;
-	std::array<int, ARRAY_SIZE> test2 ARRAY;
-	std::vector<int> test3 = ARRAY;
+	int array1[ARRAY_SIZE] = ARRAY;
+	std::array<int, ARRAY_SIZE> array2 ARRAY;
+	std::vector<int> array3 = ARRAY;
+
+	Stack<size_t> stack1;
+	Stack<double> stack2;
+	Stack<std::string> stack3;
+
+	std::cout << "Task 1: function templates" << std::endl;
 
 	std::cout << "Working on C array:" << std::endl;
-	show_array(test1, ARRAY_SIZE);
-	multiply_by_min(test1, ARRAY_SIZE);
-	show_array(test1, ARRAY_SIZE);
-	replace_zeros_by_average(test1, ARRAY_SIZE);
-	show_array(test1, ARRAY_SIZE);
+	show_array(array1, ARRAY_SIZE);
+	multiply_by_min(array1, ARRAY_SIZE);
+	show_array(array1, ARRAY_SIZE);
+	replace_zeros_by_average(array1, ARRAY_SIZE);
+	show_array(array1, ARRAY_SIZE);
 
 	std::cout << "Working on C++ array:" << std::endl;
-	show_array(test2, ARRAY_SIZE);
-	multiply_by_min(test2, ARRAY_SIZE);
-	show_array(test2, ARRAY_SIZE);
-	replace_zeros_by_average(test2, ARRAY_SIZE);
-	show_array(test2, ARRAY_SIZE);
+	show_array(array2, ARRAY_SIZE);
+	multiply_by_min(array2, ARRAY_SIZE);
+	show_array(array2, ARRAY_SIZE);
+	replace_zeros_by_average(array2, ARRAY_SIZE);
+	show_array(array2, ARRAY_SIZE);
 
 
 	std::cout << "Working on C++ vector:" << std::endl;
-	show_array(test3, ARRAY_SIZE);
-	multiply_by_min(test3, ARRAY_SIZE);
-	show_array(test3, ARRAY_SIZE);
-	replace_zeros_by_average(test3, ARRAY_SIZE);
-	show_array(test3, ARRAY_SIZE);
+	show_array(array3, ARRAY_SIZE);
+	multiply_by_min(array3, ARRAY_SIZE);
+	show_array(array3, ARRAY_SIZE);
+	replace_zeros_by_average(array3, ARRAY_SIZE);
+	show_array(array3, ARRAY_SIZE);
+
+	std::cout << std::endl << "Task 2: class templates" << std::endl;
+
+	std::cout << "Working on stack of machine size values:" << std::endl;
+	stack1.push(0xFF << 16);
+	stack1.push(0b00001100 >> 2);
+	stack1.pop();
+	std::cout << stack1.pop() << std::endl;
+
+	std::cout << "Working on stack of"
+		" double-precision floating-point numbers:" << std::endl;
+	stack2.push(12.33 * 0.004);
+	stack2.push(133.0/12.1);
+	stack2.pop();
+	std::cout << stack2.pop() << std::endl;
+
+	std::cout << "Working on stack of strings:" << std::endl;
+	stack3.push("I am a cookie.");
+	stack3.push("I am not a cookie.");
+	stack3.pop();
+	std::cout << stack3.pop() << std::endl;
 
 	return EXIT_SUCCESS;
 }
--- semester 4/programming languages/04-25-2019/templates.hpp
+++ semester 4/programming languages/04-25-2019/templates.hpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <cstdlib>
+
 template<class T>
 void multiply_by_min(T& array, size_t len);
 
@@ -6,3 +9,14 @@ void replace_zeros_by_average(T& array, size_t len);
 
 template<class T>
 void show_array(T& array, size_t len);
+
+template<class T>
+class Stack
+{
+	private:
+		std::vector<T> stack;
+
+	public:
+		void push(T const&);
+		T pop();
+};
