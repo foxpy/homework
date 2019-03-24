a198ef9dade5eb2f72b924c507470b9dc082aa53
Murad <muradkarammaev@gmail.com>
2019-03-24 12:35:16 +0300

    implement my own exception

--- semester 4/programming languages/05-01-2019/1.cpp
+++ semester 4/programming languages/05-01-2019/1.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <cmath>
-#include <stdexcept>
+#include <exception>
 
 #define GOOD_TRIANGLE 3, 4, 5
 #define BAD_TRIANGLE 10, 4, 2
+
+struct invalid_triangle_exception : public std::exception {
+	const char* what() const throw() {
+		return "invalid triangle";
+	}
+};
 
 double area_of_triangle_no_exceptions(double a, double b, double c)
 {
@@ -21,6 +27,16 @@ double area_of_triangle_exceptions(double a, double b, double c)
 	return result;
 }
 
+double area_of_triangle_my_exceptions(double a, double b, double c)
+{
+	double s = (a+b+c) / 2;
+	double result = std::sqrt(s * (s-a) * (s-b) * (s-c));
+	if (std::isnan(result)) {
+		throw invalid_triangle_exception();
+	}
+	return result;
+}
+
 int main()
 {
 	std::cout << "No exceptions:" << std::endl;
@@ -31,11 +47,21 @@ int main()
 	try {
 		std::cout << area_of_triangle_exceptions(GOOD_TRIANGLE) << std::endl;
 	}
-	catch (std::invalid_argument) {}
+	catch (std::exception) {}
 	try {
 		std::cout << area_of_triangle_exceptions(BAD_TRIANGLE) << std::endl;
 	}
-	catch (std::invalid_argument) {}
+	catch (std::exception) {}
+
+	std::cout << std::endl << "My exceptions:" << std::endl;
+	try {
+		std::cout << area_of_triangle_exceptions(GOOD_TRIANGLE) << std::endl;
+	}
+	catch (std::exception) {}
+	try {
+		std::cout << area_of_triangle_exceptions(BAD_TRIANGLE) << std::endl;
+	}
+	catch (std::exception) {}
 
 	return EXIT_SUCCESS;
 }
