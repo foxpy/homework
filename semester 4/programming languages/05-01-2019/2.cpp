3a0fd7c42dcc4cd2531cdfed0e48bd3039c20824
Murad <muradkarammaev@gmail.com>
2019-03-26 04:48:32 +0300

    implement stack with custom exceptions

--- semester 4/programming languages/05-01-2019/2.cpp
+++ semester 4/programming languages/05-01-2019/2.cpp
@@ -21,8 +21,30 @@ T Stack<T>::pop() {
 	return stack[sp--];
 }
 
+template<class T>
+void MyStack<T>::push(T const& element)
+{
+	if (++sp > STACK_SIZE) {
+		--sp;
+		throw stack_overflow();
+	}
+	stack[sp] = element;
+}
+
+template<class T>
+T MyStack<T>::pop() {
+	if (sp == 0) {
+		throw stack_overflow();
+	}
+	return stack[sp--];
+}
+
 int main() {
 	Stack<double> stack;
+	MyStack<double> my_stack;
+
+
+	std::cout << "Stack implementation with standard exceptions:" << std::endl;
 
 	stack.push(2.4);
 	stack.push(12.4);
@@ -34,6 +56,22 @@ int main() {
 		std::cout << "Looks like we can!" << std::endl;
 	}
 	catch (std::overflow_error& e) {
+		std::cout << "No.. We cannot." << std::endl;
+	}
+
+
+	std::cout << std::endl << "Stack implementation with custom exceptions:" << std::endl;
+
+	my_stack.push(2.4);
+	my_stack.push(12.4);
+	my_stack.pop();
+	std::cout << my_stack.pop() << std::endl;
+	try {
+		std::cout << "Let's see if we actually can pop under stack..." << std::endl;;
+		my_stack.pop();
+		std::cout << "Looks like we can!" << std::endl;
+	}
+	catch (stack_overflow& e) {
 		std::cout << "No.. We cannot." << std::endl;
 	}
 
--- semester 4/programming languages/05-01-2019/2.hpp
+++ semester 4/programming languages/05-01-2019/2.hpp
@@ -1,10 +1,29 @@
 #include <array>
 #include <cstdlib>
+#include <exception>
 
 #define STACK_SIZE 0xFF
 
 template<class T>
 class Stack
+{
+	private:
+		std::array<T, STACK_SIZE> stack = {0};
+		size_t sp = 0; // Stack Pointer
+
+	public:
+		void push(T const&);
+		T pop();
+};
+
+struct stack_overflow : public std::exception {
+	const char* what() const throw() {
+		return "stack overflow";
+	}
+};
+
+template<class T>
+class MyStack
 {
 	private:
 		std::array<T, STACK_SIZE> stack = {0};
