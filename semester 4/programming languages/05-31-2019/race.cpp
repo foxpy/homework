80ae153ab024f5cd13a00e7ac957710be98714db
Murad <muradkarammaev@gmail.com>
2019-05-31 17:34:18 +0300

    invalid data handling

--- semester 4/programming languages/05-31-2019/race.cpp
+++ semester 4/programming languages/05-31-2019/race.cpp
@@ -1,3 +1,4 @@
+#include <stdexcept>
 #include "race.hpp"
 
 Bolide::Bolide(std::string name,
@@ -9,6 +10,27 @@ Bolide::Bolide(std::string name,
 		bool traction_control,
 		double mass,
 		double downforce) {
+	if (max_velocity <= 0) {
+		throw std::invalid_argument(
+			"Maximum velocity should be positive");
+	}
+	if (acceleration <= 0) {
+		throw std::invalid_argument(
+			"Acceleration should be positive");
+	}
+	if (deceleration <= 0) {
+		throw std::invalid_argument(
+			"Deceleration should be positive");
+	}
+	if (mass <= 0) {
+		throw std::invalid_argument(
+			"Mass should be positive");
+	}
+	if (downforce <= 0) {
+		throw std::invalid_argument(
+			"Downforce should be positive");
+	}
+
 	this->max_velocity = max_velocity;
 	this->acceleration = acceleration;
 	this->deceleration = deceleration;
@@ -22,6 +44,14 @@ Bolide::Bolide(std::string name,
 Race::Race(double track_length,
 	   unsigned num_of_turns,
 	   double friction_coefficient) {
+	if (track_length <= 0) {
+		throw std::invalid_argument(
+			"Track length should be positive");
+	}
+	if (!(friction_coefficient >= 0) && (friction_coefficient <= 1)) {
+		throw std::invalid_argument("Friction coefficient should be in rage [0; 1]");
+	}
+
 	this->track_length = track_length;
 	this->num_of_turns = num_of_turns;
 	this->friction_coefficient = friction_coefficient;
