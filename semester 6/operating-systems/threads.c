a79d3518df1845fa293f604387c288ffccf1a36d
Murad <muradkarammaev@gmail.com>
2020-03-20 15:50:42 +0300

    handle command line arguments

--- semester 6/operating-systems/threads.c
+++ semester 6/operating-systems/threads.c
@@ -1,22 +1,47 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <pthread.h>
 
 #include "util.h"
 #include "writer.h"
 #include "reader.h"
 
-#define BUFFER_SIZE 0x100000
-#define ITERATIONS 8
-#define SLEEP_INTERVAL_NSECS 10000
+#define DEFAULT_BUFFER_SIZE 0x100000
+#define DEFAULT_SLEEP_INTERVAL_NSECS 10000
+#define DEFAULT_ITERATIONS 8
 
-int main(void) {
-	uint8_t *data = (uint8_t*) calloc(BUFFER_SIZE, sizeof(uint8_t));
+#define die(msg) { fputs(msg, stderr); return EXIT_FAILURE; }
+static void usage(const char *program_name);
+
+int main(int argc, char *argv[]) {
+	if (argc != 4 && argc != 1) {
+		usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	size_t buffer_size;
+	unsigned sleep_interval;
+	unsigned iterations;
+	if (argc == 4) {
+		if ((buffer_size = (size_t) (atoll(argv[1]))) == 0)
+			die("Fatal error: expected positive integer SIZE\n");
+		if ((sleep_interval = (unsigned) (atol(argv[2]))) == 0)
+			die("Fatal error: expected positive integer INTERVAL\n");
+		if ((iterations = (unsigned) (atol(argv[3]))) == 0)
+			die("Fatal error: expected positive integer ITERATIONS\n");
+	} else {
+		buffer_size = DEFAULT_BUFFER_SIZE;
+		sleep_interval = DEFAULT_SLEEP_INTERVAL_NSECS;
+		iterations = DEFAULT_ITERATIONS;
+	}
+
+	uint8_t *data = (uint8_t*) calloc(buffer_size, sizeof(uint8_t));
 	struct thread_data args = {
 		data,
-		BUFFER_SIZE,
-		SLEEP_INTERVAL_NSECS,
-		ITERATIONS,
+		buffer_size,
+		sleep_interval,
+		iterations,
 		PTHREAD_MUTEX_INITIALIZER,
 	};
 	pthread_t threads[2];
@@ -25,6 +50,14 @@ int main(void) {
 	pthread_create(&threads[1], NULL, (void*) continious_reader, &args);
 	pthread_join(threads[0], NULL);
 	pthread_join(threads[1], NULL);
+
 	free(data);
 	return EXIT_SUCCESS;
+}
+
+void usage(const char* program_name) {
+	fprintf(stderr, "Usage: %s [SIZE INTERVAL ITERATIONS]\n", program_name);
+	fputs("\tSIZE - size of memory buffer\n", stderr);
+	fputs("\tINTERVAL - sleep interval between memory operations\n", stderr);
+	fputs("\tITERATIONS - number of iterations per thread\n", stderr);
 }
