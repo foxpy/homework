d714bdc5040436c64183f0ae137cf36c94f1fd88
Murad <muradkarammaev@gmail.com>
2020-04-14 12:55:21 +0300

    implement heap like a dumbed vector

--- semester 6/programming-methods/heap-template/heap.hpp
+++ semester 6/programming-methods/heap-template/heap.hpp
@@ -1,7 +1,48 @@
 #pragma once
+#include <cstddef>
 
 template<class T>
 struct heap {
+public:
+	heap() {
+		capacity = 1;
+		length = 0;
+		data = new T[capacity];
+	}
+	~heap() {
+		delete[] data;
+	}
+
+	bool empty() {
+		return length == 0;
+	}
+
+	void push(const T &t) {
+		if (length < capacity) {
+			data[length] = t;
+		} else {
+			grow();
+			data[length] = t;
+		}
+		++length;
+	}
+
+	T& top() {
+		return data[0];
+	}
 private:
+	void grow() {
+		std::size_t old_capacity = capacity;
+		capacity *= 2;
+		T *old_data = data;
+		T *new_data = new T[capacity];
+		for (std::size_t i = 0; i < old_capacity; ++i) {
+			new_data[i] = old_data[i];
+		}
+		delete[] data;
+		data = new_data;
+	}
+	std::size_t capacity;
+	std::size_t length;
 	T *data;
 };
--- semester 6/programming-methods/heap-template/test.cpp
+++ semester 6/programming-methods/heap-template/test.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <array>
 #include <queue>
+#include <cassert>
 #include "heap.hpp"
 
 int main() {
 	std::array<int, 8> input = { 4, 2, 9, 6, 1, 8, 13, 2 };
 	std::priority_queue<int> stdq;
 	heap<int> myq;
-	for (auto i : input)
+
+	assert(stdq.empty());
+	assert(myq.empty());
+
+	stdq.push(7);
+	myq.push(7);
+	assert(!stdq.empty());
+	assert(!myq.empty());
+	assert(stdq.top() == 7);
+	assert(myq.top() == 7);
+
+	for (auto i : input) {
 		stdq.push(i);
+		myq.push(i);
+	}
+
 	while (!stdq.empty()) {
 		std::cout << stdq.top() << ' ';
 		stdq.pop();
