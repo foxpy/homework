728f6046bbaaddcbedb528932ffc03430df14477
Murad <muradkarammaev@gmail.com>
2020-04-16 11:58:54 +0300

    include tests for various data types

--- semester 6/programming-methods/heap-template/test.cpp
+++ semester 6/programming-methods/heap-template/test.cpp
@@ -4,7 +4,7 @@
 #include <cassert>
 #include "heap.hpp"
 
-int main() {
+void test_int() {
 	std::array<int, 8> input = { 4, 2, 9, 6, 1, 8, 13, 2 };
 	std::priority_queue<int> stdq;
 	heap<int> myq;
@@ -37,4 +37,80 @@ int main() {
 		myq.pop();
 	}
 	std::cout << std::endl;
+}
+
+void test_char() {
+	std::array<char, 8> input = { 'a', 's', 'q', '0', '1', '8', 'p', 'c' };
+	std::priority_queue<char> stdq;
+	heap<char> myq;
+
+	assert(stdq.empty());
+	assert(myq.empty());
+
+	stdq.push('x');
+	myq.push('x');
+	assert(!stdq.empty());
+	assert(!myq.empty());
+	assert(stdq.top() == 'x');
+	assert(myq.top() == 'x');
+
+	for (auto i : input) {
+		stdq.push(i);
+		myq.push(i);
+	}
+
+	assert(stdq.size() == 9);
+	assert(myq.size() == 9);
+
+	while (!stdq.empty()) {
+		std::cout << stdq.top() << ' ';
+		stdq.pop();
+	}
+	std::cout << std::endl;
+	while (!myq.empty()) {
+		std::cout << myq.top() << ' ';
+		myq.pop();
+	}
+	std::cout << std::endl;
+}
+
+void test_float() {
+	std::array<float, 8> input = { 4.6, 7.5, 1.0, -0.4, 19.6, 83762.3093, -62.4, 0.01 };
+	std::priority_queue<float> stdq;
+	heap<float> myq;
+
+	assert(stdq.empty());
+	assert(myq.empty());
+
+	stdq.push(3.0);
+	myq.push(3.0);
+	assert(!stdq.empty());
+	assert(!myq.empty());
+	assert(stdq.top() == 3.0);
+	assert(myq.top() == 3.0);
+
+	for (auto i : input) {
+		stdq.push(i);
+		myq.push(i);
+	}
+
+	assert(stdq.size() == 9);
+	assert(myq.size() == 9);
+
+	while (!stdq.empty()) {
+		std::cout << stdq.top() << ' ';
+		stdq.pop();
+	}
+	std::cout << std::endl;
+	while (!myq.empty()) {
+		std::cout << myq.top() << ' ';
+		myq.pop();
+	}
+	std::cout << std::endl;
+}
+
+int main() {
+	test_int();
+	test_char();
+	test_float();
 }
