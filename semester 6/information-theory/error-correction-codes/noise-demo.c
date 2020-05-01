aaf1e13f71bf1f732c7c59f7d6a3d44bb6ee1680
Murad <muradkarammaev@gmail.com>
2020-05-01 11:07:46 +0300

    fix small typo

--- semester 6/information-theory/error-correction-codes/noise-demo.c
+++ semester 6/information-theory/error-correction-codes/noise-demo.c
@@ -7,17 +7,17 @@ int main() {
 	char message[] = "The quick brown fox jumps over the lazy dog";
 	noise_t ntype ;
 	noise_cfg_t nconfig;
-	printf("Original message:                                          %s\n", message);
+	printf("Original message:                                        %s\n", message);
 
 	ntype = NOISE_BIT_FLIP;
 	nconfig.bit_flip.num_bit_flips = 2;
 	apply_noise(message, strlen(message), ntype, nconfig);
-	printf("This is how message looks after applying bit flip noise:   %s\n", message);
+	printf("This is how message looks after applying bit flip noise: %s\n", message);
 
 	ntype = NOISE_LINEAR;
 	nconfig.linear.damage_length = 3;
 	apply_noise(message, strlen(message), ntype, nconfig);
-	printf("This is how message looks after applying bit linear noise: %s\n", message);
+	printf("This is how message looks after applying linear noise:   %s\n", message);
 
 	return EXIT_SUCCESS;
 }
