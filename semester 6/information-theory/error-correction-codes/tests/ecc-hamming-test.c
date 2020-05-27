cbb42aaf87aec4c495f1230109fb4228c9d2cfba
Murad <muradkarammaev@gmail.com>
2020-05-27 23:43:14 +0300

    implement Hamming decoding function

--- semester 6/information-theory/error-correction-codes/tests/ecc-hamming-test.c
+++ semester 6/information-theory/error-correction-codes/tests/ecc-hamming-test.c
@@ -3,7 +3,7 @@
 #include "bit-array.h"
 #include "hamming-code.h"
 
-int main() {
+void test_encoding() {
 	bit input_cases[] = {
 		0, 0, 0, 0,
 		1, 1, 1, 1,
@@ -28,5 +28,37 @@ int main() {
 	for (int i = 0; i < 32; ++i) assert(bitarray_next_front(&out) == output_cases[i]);
 	bitarray_free(&in);
 	bitarray_free(&out);
+}
+
+void test_decoding() {
+	bit input_cases[] = {
+		0, 0, 0, 0, 0, 1, 0, 0,
+		1, 1, 0, 1, 1, 1, 1, 1,
+		0, 1, 1, 1, 1, 1, 0, 0,
+		0, 0, 1, 1, 0, 1, 0, 0,
+	};
+	bit output_cases[] = {
+		0, 0, 0, 0,
+		1, 1, 1, 1,
+		0, 1, 1, 0,
+		1, 0, 1, 1,
+	};
+	bits_t in, out;
+	bitarray_alloc(&in);
+	bitarray_alloc(&out);
+	for (int i = 0; i < 32; ++i) bitarray_push_back(&in, input_cases[i]);
+	bitarray_push_back(&in, 0);
+	hamming_decode(&out, &in);
+	assert(bitarray_size(&in) == 1);
+	assert(bitarray_back(&in) == 0);
+	assert(bitarray_size(&out) == 16);
+	for (int i = 0; i < 16; ++i) assert(bitarray_next_front(&out) == output_cases[i]);
+	bitarray_free(&in);
+	bitarray_free(&out);
+}
+
+int main() {
+	test_encoding();
+	test_decoding();
 	return EXIT_SUCCESS;
 }
