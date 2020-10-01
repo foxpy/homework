d1e4684596c891b37a6e8d73bffe3835cee4b301
Murad <muradkarammaev@gmail.com>
2020-10-01 20:17:28 +0300

    reformat code

--- semester 7/formal-languages/detect.cpp
+++ semester 7/formal-languages/detect.cpp
@@ -3,13 +3,13 @@
 #include "detecter.hpp"
 
 int main() {
-	std::string input;
-	std::getline(std::cin, input);
-	if (corresponds(input)) {
-	    std::cout << "YES" << std::endl;
-	    std::exit(EXIT_SUCCESS);
-	} else {
-	    std::cout << "NO" << std::endl;
-	    std::exit(EXIT_FAILURE);
-	}
+    std::string input;
+    std::getline(std::cin, input);
+    if (corresponds(input)) {
+        std::cout << "YES" << std::endl;
+        std::exit(EXIT_SUCCESS);
+    } else {
+        std::cout << "NO" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
 }
